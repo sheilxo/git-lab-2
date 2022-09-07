54a01890ca489494cc7e3d37449d6fe6fff1b2f6
sheilxo <112518121+sheilxo@users.noreply.github.com>
2022-09-07 19:33:03 -0400

    Add files via upload

---
+++ git-lab-2-program.cc
@@ -0,0 +1,22 @@
+
+/*
+ *        File: git-lab-2-program.cc
+ *      Author: Sheil Iyer
+ *        Date: 9/7/22
+ * Description: Add Description
+ */
+
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+ {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+
+}// main
